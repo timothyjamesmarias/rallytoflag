7ce3ceea20ac36c7eb7ffe96b0414b8a4b374452
timothyjamesmarias <timothyjamesmarias@gmail.com>
2022-12-06 14:49:49 -0800

    created models

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,6 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
     },
