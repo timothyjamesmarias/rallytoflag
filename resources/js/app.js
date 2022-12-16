18629681088841de193558e7c68b297b0d03e297
timothyjamesmarias <timothyjamesmarias@gmail.com>
2022-12-15 18:11:31 -0800

    fixed map positioning

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,7 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    zoom: 5 // starting zoom
 });
 
 const lightbox = new PhotoSwipeLightbox({
--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,3 +1,5 @@
 <x-layouts.app>
+<div id="map-container">
 <div id="map" ></div>
+</div>
 </x-layouts.app>
