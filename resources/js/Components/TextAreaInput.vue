e2e5a5c9a3dd0d7f3602ce2001c11ea51a672a7e
Timothy Marias <timothyjamesmarias@gmail.com>
2022-12-18 22:40:50 -0800

    wip

--- resources/js/Components/TextAreaInput.vue
+++ resources/js/Components/TextAreaInput.vue
@@ -1,6 +1,5 @@
 <template>
 <textarea class="border-gray-300 focus:border-violet-500 focus:ring-violet-500 rounded-md shadow-sm
- bg-white dark:bg-neutral-200 drop-shadow-sm dark:drop-shadow-none"
- <slot />
+ bg-white dark:bg-neutral-200 drop-shadow-sm dark:drop-shadow-none">
 </textarea>
 </template>
