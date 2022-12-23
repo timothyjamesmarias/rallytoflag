3c0d932a8b396f2c893b62e699db31b7815fa6e8
Timothy Marias <timothyjamesmarias@gmail.com>
2022-12-22 17:53:52 -0800

    styles wip

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,15 +3,27 @@ import { Head } from '@inertiajs/inertia-vue3';
 import EventTableRow from '@/Components/EventTableRow.vue';
 import Card from '@/Components/Card.vue';
 import Paginator from '@/Components/Paginator.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 const props = defineProps({
     user: Object,
     events: Object,
 });
+
+const createEvent = () => {
+    console.log('create');
+};
 </script>
 
 <template>
 <Head title="Dashboard" />
-
+<div class="flex flex-col items-center pt-6">
+  <div class="w-full lg:md:max-w-4xl sm:max-w-md">
+    <div class="flex justify-between">
+    <h1 class="text-xl font-bold">My Dashboard</h1>
+    <PrimaryButton><Link :href="route('profile.edit')" v-html="'Account Settings'"/></PrimaryButton>
+    </div>
+</div>
+</div>
 <Card class="text-left">
   <h2 class="text-xl font-bold">My Events</h2>
 
--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -1,9 +1,8 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import DeleteUserForm from './Partials/DeleteUserForm.vue';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
-import { Head } from '@inertiajs/inertia-vue3';
+import Card from '@/Components/Card.vue';
 
 defineProps({
     mustVerifyEmail: Boolean,
@@ -21,21 +20,21 @@ defineProps({
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <Card class="text-left">
                     <UpdateProfileInformationForm
                         :must-verify-email="mustVerifyEmail"
                         :status="status"
                         class="max-w-xl"
                     />
-                </div>
+                </Card>
 
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <Card class="text-left">
                     <UpdatePasswordForm class="max-w-xl" />
-                </div>
+                </Card>
 
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <Card class="text-left">
                     <DeleteUserForm class="max-w-xl" />
-                </div>
+                </Card>
             </div>
         </div>
     </AuthenticatedLayout>
