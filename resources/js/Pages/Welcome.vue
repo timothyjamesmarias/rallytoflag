11532a36f455ccd4828827cfdcbb4f6ffeaf0981
Timothy Marias <timothyjamesmarias@gmail.com>
2022-12-29 13:44:48 -0800

    welcome page and readme wip

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,28 @@
 <script setup>
+import { useForm } from '@inertiajs/inertia-vue3'
+import Input from '@/Components/Input.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import Card from '@/Components/Card.vue';
+import { ref } from 'vue';
+
+const form = useForm({
+  query: '',
+});
+
+const search = () => {
+}
 </script>
 
 <template>
-    <Head title="Welcome" />
+  <Head title="Welcome" />
+    
+<Card class="min-h-screen">
+  <span class="inline-flex items-center">
+    <p class="text-lg pr-4">I am looking for events near</p>
+    <form @submit.prevent="search">
+      <Input type="text" v-model="query" />
+      <PrimaryButton @submit.prevent="search" class="ml-4">Search</PrimaryButton>
+    </form>
+  </span>
+</Card>
 </template>
