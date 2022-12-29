b75354a056a84cd5c701544775af0ec98eb4d323
Timothy Marias <timothyjamesmarias@gmail.com>
2022-12-29 13:04:17 -0800

    added sun/moon toggle to dark mode toggle

--- resources/js/Components/DarkModeToggle.vue
+++ resources/js/Components/DarkModeToggle.vue
@@ -1,11 +1,17 @@
 <script setup>
+import { ref } from 'vue';
 import { useDark, useToggle } from '@vueuse/core';
-const isDark = useDark();
+let isDark = ref(useDark());
 const toggleDark = useToggle(isDark);
 </script>
 
 <template>
   <button id="dark-mode-toggle" @click="toggleDark()">
-    <font-awesome-icon icon="fa-solid fa-moon"  class="text-2xl text-sky-700 hover:text-sky-600 dark:text-amber-500 hover:dark:text-amber-400"/>
+    <template v-if="isDark">
+      <font-awesome-icon icon="fa-solid fa-sun" class="text-2xl dark:text-amber-500 hover:dark:text-amber-400"/>
+    </template>
+    <template v-else>
+      <font-awesome-icon icon="fa-solid fa-moon" class="text-2xl text-sky-700 hover:text-sky-600"/>
+    </template>
   </button>
 </template>
