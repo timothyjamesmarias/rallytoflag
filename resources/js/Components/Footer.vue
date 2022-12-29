d20d556f82f0d3afa70060885d70c39e23bb4318
Timothy Marias <timothyjamesmarias@gmail.com>
2022-12-29 12:29:53 -0800

    added collapsible navbar functionality

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -20,7 +20,7 @@ const links = [
 ];
 </script>
 <template>
-<div class="p-4 flex align-center items-center justify-center w-full bg-stone-200 dark:bg-stone-800">
+<div class="p-4 flex align-center items-center justify-center w-full">
     <NavLink v-for="link in links" :href="link.href" class="px-4 sm:px-2">{{ link.name }}</NavLink>
 </div>
 </template>
--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Inertia } from '@inertiajs/inertia'
+import { ref } from 'vue'
 import NavLink from '@/Components/NavLink.vue';
 import DarkModeToggle from '@/Components/DarkModeToggle.vue';
 
@@ -7,17 +8,12 @@ const logout = () => {
   Inertia.post(route('logout'));
 };
 
-let showMenu = false;
-
-const toggleMenu = () => {
-  showMenu = !showMenu;
-};
+let showMenu = ref(false);
 
 </script>
 <template>
-<nav class="sticky top-0 z-10 flex items-center justify-between text-xl p-6  bg-stone-100 dark:bg-stone-900
-opacity-95 backdrop-blur-4xl">
-
+<nav class="sticky top-0 z-10 text-xl p-6  bg-stone-100 dark:bg-stone-900 opacity-95 backdrop-blur-4xl">
+<div class="flex items-center justify-between">
   <Link href="/" class="text-3xl font-logo select-none font-bold text-slate-900 dark:text-slate-100">
     Rally To Flag
   </Link>
@@ -51,35 +47,36 @@ opacity-95 backdrop-blur-4xl">
 
   <div class="flex items-center xl:lg:md:hidden">
     <DarkModeToggle class="mr-4"/>
-    <button @click="toggleMenu">
+    <button @click="showMenu = !showMenu">
       <font-awesome-icon icon="fa-solid fa-bars" class="text-2xl"/>
     </button>
   </div>
-  <div id="menu" :class="showMenu ? 'block' : 'hidden'" class="relative z-2 w-full bg-black">
-    <div class="absolute">
-    <NavLink :href="'/events'" class="block">
-    Events
-    </NavLink>
-    <template v-if="$page.props.auth.user">
-    <NavLink :href="'/dashboard'" class="block">
-    Dashboard
-    </NavLink>
-    <form method="POST" @submit.prevent="logout" class="">
-      <button type="submit" class="hover:text-violet-300">
-          Logout
-      </button>
-    </form>
-    </template>
+</div>
+<div id="menu" :class="{ hidden: !showMenu, 'inline-flex': showMenu}" class="w-full sm:items-center sm:w-auto">
+  <div class="flex-grow">
+  <NavLink :href="'/events'" class="block">
+  Events
+  </NavLink>
+  <template v-if="$page.props.auth.user">
+  <NavLink :href="'/dashboard'" class="block">
+  Dashboard
+  </NavLink>
+  <form method="POST" @submit.prevent="logout" class="">
+    <button type="submit" class="hover:text-violet-300">
+        Logout
+    </button>
+  </form>
+  </template>
 
-    <template v-else>
-    <NavLink :href="'/login'" class="block">
-    Login
-    </NavLink>
-    <NavLink :href="'/register'" class="block">
-    Register
-    </NavLink>
-    </template>
-    </div>
-  </div>
+  <template v-else>
+  <NavLink :href="'/login'" class="block">
+  Login
+  </NavLink>
+  <NavLink :href="'/register'" class="block">
+  Register
+  </NavLink>
+  </template>
+</div>
+</div>
 </nav>
 </template>
